a5aa401850a2fe8b634caf5ed298fdd7856eb3cf
Fabricio Andrade <fabricio.solutions13@gmail.com>
2024-05-19 21:56:42 +0200

    JS

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
 //variaveis
 
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+const Modal = {
+
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .title span'),
+  buttonClose: document.querySelector('.modal button.close'),
+
+  open(){
+    Modal.wrapper.classList.add('open')
+  },
+  close(){
+    Modal.wrapper.classList.remove('open')
+  }
+}
+
+form.onsubmit = function(event) {
+  event.preventDefault()
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+
+  const result = IMC(weight, height)
+  const message = `Seu IMC e de ${result}`
+ Modal.message.innerText = message
+  //modalWrapper.classList.add('open')
+  Modal.open()
+}
+
+Modal.buttonClose.onclick = () => {
+  //modalWrapper.classList.remove('open')
+  Modal.close()
+}
+
+function IMC(weight,height) {
+  return (weight/((height / 100) ** 2)).toFixed(2)
+}
+
